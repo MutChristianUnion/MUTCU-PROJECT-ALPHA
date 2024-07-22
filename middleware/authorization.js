e56c199a31960083a9e115323ea5d4a2cb798100
34ERAT <philipkanuti@gmail.com>
2024-07-22 09:53:10 +0300

    refactore of the authoriztion code

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -2,34 +2,21 @@ require("dotenv").config();
 const jwt = require("jsonwebtoken");
 class Authorise {
   Access_token = (user) => {
-    console.log("gerattion accessToken")
+    console.log("gerattion accessToken");
     return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
       expiresIn: "15m",
     });
   };
 
-  Refresh_token=(user) =>{
+  Refresh_token = (user) => {
     return jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, {
       expiresIn: process.env.EX_DATE,
     });
-  }
+  };
+  Renew_token = (RefreshToken,response) =>{
 
-  Authenticate = (req, res, next)=> {
-    const AccessToken = req.headers["authorization"].split(" ")[1];
-    const RefreshToken = req.cookies["refresh_token"];
-    if (!AccessToken && !RefreshToken)
-      return res.status(401).send("Access Denied.  No token provided.");
-    console.log({ accessToken: AccessToken, RefreshToken: RefreshToken });
-    try {
-      // console.log(process.env.ACCESS_TOKEN_SECRET)
-      jwt.verify(AccessToken, process.env.ACCESS_TOKEN_SECRET);
-      next();
-    } catch (error) {
-      // console.log("access_token :::", error);
-      if (!error instanceof jwt.TokenExpiredError)
-        return res.status(400).json("invalid AccessToken");
       if (!RefreshToken)
-        return res
+        return response
           .status(401)
           .send("Access Denied.  No  Refresh token provided.");
       try {
@@ -40,16 +27,36 @@ class Authorise {
         console.log(decode.user);
         const user = decode.user;
         const AccessToken = this.Access_token({ user: user });
-        res
-          .cookie("refresh_token", RefreshToken,{ httpOnly: true, sameSite: 'strict' })
+        response
+          .cookie("refresh_token", RefreshToken, {
+            httpOnly: true,
+            sameSite: "strict",
+          })
           .header("Authorization", AccessToken)
           .json("you have all of you data safe");
       } catch (error) {
-        console.error(error);
-        res.status(400).json("invalid RefreshToken");
+        // console.error(error);
+        response.status(400).json("invalid RefreshToken");
       }
-    }
+
   }
+
+  Authenticate = (req, res, next) => {
+    const AccessToken = req.headers["authorization"]?.split(" ")[1];
+    const RefreshToken = req.cookies["refresh_token"];
+    if (!AccessToken)
+      return res.status(401).send("Access Denied.  No AccessToken token provided.");
+    console.log({ accessToken: AccessToken, RefreshToken: RefreshToken });
+    try {
+      // console.log(process.env.ACCESS_TOKEN_SECRET)
+      jwt.verify(AccessToken, process.env.ACCESS_TOKEN_SECRET);
+      next();
+    } catch (error) {
+      if (!error instanceof jwt.TokenExpiredError)
+        return res.status(400).json("invalid AccessToken");
+      this.Renew_token(RefreshToken,res)
+    }
+  };
 }
 
 module.exports = new Authorise();
